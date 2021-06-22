4f674421e3cebe2d4505eca349ad4ab9cf8983ce
murilinhops <murilinhorps@gmail.com>
2021-06-22 19:45:20 -0300

    Move routes and view_models to core folder, they're the more important to the app

--- lib/src/application/core/routes/app_router.dart
+++ lib/src/application/core/routes/app_router.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/cupertino.dart';
-import 'package:pay_flow/src/application/routes/routes_names.dart';
+import 'package:pay_flow/src/application/core/routes/routes_names.dart';
 import 'package:pay_flow/src/application/modules/home/home_module.dart';
 import 'package:pay_flow/src/application/modules/login/login_module.dart';
-import 'package:pay_flow/src/application/routes/pay_flow_page_route.dart';
+import 'package:pay_flow/src/application/core/routes/pay_flow_page_route.dart';
 import 'package:pay_flow/src/application/modules/scan_bar_code/scan_bar_code_module.dart';
 import 'package:pay_flow/src/application/modules/create_bank_slip/create_bank_slip_module.dart';
 
 class AppRouter {
-  static Route<dynamic> generateRoutes(RouteSettings settings) {
+  static Route<dynamic>? generateRoutes(RouteSettings settings) {
     switch (settings.name) {
       case RoutesNames.homePage:
         return PayFlowPageRoute(
           route: RoutesNames.homePage,
           builder: (context) => HomeModule(),
         );
-
       case RoutesNames.loginPage:
         return PayFlowPageRoute(
           route: RoutesNames.loginPage,
@@ -29,11 +28,6 @@ class AppRouter {
         return PayFlowPageRoute(
           route: RoutesNames.createBankSlip,
           builder: (context) => CreateBankSlipModule(),
-        );
-      default:
-        return PayFlowPageRoute(
-          route: RoutesNames.homePage,
-          builder: (context) => HomeModule(),
         );
     }
   }
