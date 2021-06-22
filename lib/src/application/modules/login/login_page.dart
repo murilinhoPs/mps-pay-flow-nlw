02b136358deb00bd4aa51b243c6d54977630c63f
murilinhops <murilinhorps@gmail.com>
2021-06-22 19:47:16 -0300

    Create LoginPage

--- lib/src/application/modules/login/login_page.dart
+++ lib/src/application/modules/login/login_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pay_flow/src/utils/strings.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:pay_flow/src/application/theme/app_assets.dart';
+import 'package:pay_flow/src/application/theme/app_colors.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:pay_flow/src/application/core/routes/routes.dart';
+import 'package:pay_flow/src/application/theme/app_gradients.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -11,8 +19,61 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login'),
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Container(
+            height: 315,
+            decoration: BoxDecoration(
+              gradient: AppGradients.radialHome,
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    AppAssets.person,
+                    height: 315,
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SvgPicture.asset(
+                      AppAssets.logo,
+                      height: 44,
+                    ),
+                  ),
+                  Text(
+                    Strings.organizeBankSlips,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: AppColors.textHeading,
+                      height: 1.44,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  SignInButton(
+                    Buttons.Google,
+                    onPressed: () {
+                      print('google login');
+                      Navigator.pushReplacementNamed(
+                        context,
+                        RoutesNames.homePage,
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
